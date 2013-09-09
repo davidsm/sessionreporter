6cad6b26efa080504cc85f251f1bcb547909ba2b
david <david.smitmanis@aveso.se>
2013-09-09 13:42:00 +0200

    adds jinja template support, basic html template

--- reporter.py
+++ reporter.py
@@ -1,57 +1,57 @@
 #from reportdb import ReportDb
 import codecs, datetime
+from jinja2 import Environment, FileSystemLoader
 
 class Report():
     def __init__(self, sessioninfo, notes):
+        self.template_env = Environment(loader=FileSystemLoader('templates'))
+        self.template = self.template_env.get_template("basic_html_report.template")
+
         self.sessioninfo = sessioninfo
         self.notes = notes
         self.session_id = self.sessioninfo[0]
-        self.reportname = ""
-        
-        self.header = '''<html>\n<meta http-equiv="Content-type" content="text/html;charset=UTF-8">'''
-        self.footer = "</html>"
 
-        self.charterheading = "Charter"
-        self.datetimeheading = "Datum och tid"
-        self.reporterheading = "Testare"
-        self.lengthheading = "Varaktighet"
-        self.noteheading = "Anteckningar"
-        self.bugsheading = "Buggar"
+        self.variables = {}
+        
+        headings = {}
+        headings["charter"] = "Charter"
+        headings["datetime"] = "Datum och tid"
+        headings["reporter"] = "Testare"
+        headings["length"] = "Varaktighet"
+        headings["notes"] = "Anteckningar"
+        headings["bugs"] = "Buggar"
+        self.variables["headings"] = headings
 
     def sessionToHtml(self):
-        htmlsession = ""
         if self.sessioninfo != None:
             dateandtime = self.sessioninfo[2] + " " + self.sessioninfo[1]
             charter = self.formatStringToHtml(self.sessioninfo[3])
             reporter = self.sessioninfo[4]
             length = self.sessioninfo[5]
 
-            htmlsession += "<h2>%s</h2>\n<p>%s</p>\n" % (self.charterheading, charter)
-            htmlsession += "<h2>%s</h2>\n<p>%s</p>\n" % (self.datetimeheading, dateandtime)
-            htmlsession += "<h2>%s</h2>\n<p>%s</p>\n" % (self.reporterheading, reporter)
-            htmlsession += "<h2>%s</h2>\n<p>%s min</p>\n" % (self.lengthheading, length)
-        return htmlsession                                              
+            self.variables['charter'] = charter
+            self.variables['dateandtime'] = dateandtime
+            self.variables['reporter'] = reporter
+            self.variables['length'] = length                                            
 
     def notesToHtml(self):
-        htmlnotes = "<h2>%s</h2>\n" % self.noteheading
+        notelist = []
         for item in self.notes:
-            timestamp = item[2]
-            if item[3] == "note":
-                note = self.formatStringToHtml(item[4])
-                htmlnotes += "<h5>%s</h5>\n<p>%s</p>\n" % (timestamp, note)
-            elif item[3] == "screenshot":
-                htmlnotes += '''<h5>%s</h5>\n<img src="%s" width="600">\n''' % (timestamp, item[4])
-                        
-        return htmlnotes
+            if item[3] != "bug":
+                notelist.append({"timestamp": item[2],
+                            "type": item[3],
+                            "note": item[4].split("\n")})
+                
+        self.variables["notes"] = notelist
 
     def bugsToHtml(self):
-        bugnotes = "<h2>%s</h2>\n" % self.bugsheading
+        buglist = []
         for item in self.notes:
             if item[3] == "bug":
-                timestamp = item[2]
-                bug = self.formatStringToHtml(item[4])
-                bugnotes += "<h5>%s</h5>\n<p>%s</p>\n" % (timestamp, bug)
-        return bugnotes
+                buglist.append({"timestamp": item[2],
+                    "type": item[3],
+                    "note": item[4]})
+        self.variables["bugs"] = buglist
 
     def formatStringToHtml(self, s):
         s = s.replace("\n", "<br>")
@@ -67,14 +67,11 @@ class Report():
         self.reportname = "report-%s-%s.html" % (self.createTimeStamp(), self.session_id)
         report = codecs.open(self.reportname, "w", "utf-8")
 
-        report.write(self.header)
-        
-        report.write(self.sessionToHtml())
-        report.write(self.notesToHtml())
-        report.write(self.bugsToHtml())
+        self.sessionToHtml()
+        self.notesToHtml()
+        self.bugsToHtml()
 
-        report.write(self.footer)
-
+        report.write(self.template.render(self.variables))
         report.close()
 
     def getReportName(self):
